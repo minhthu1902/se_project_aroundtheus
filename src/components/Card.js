560e2f064d89cda3c42e226b32074547c2d68c31
Thu Thai <70996669+minhthu1902@users.noreply.github.com>
2023-12-08 21:18:04 -0600

    Fix Card instance from reviewer comment

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,19 +11,13 @@ export default class Card {
       .content.querySelector(".card")
       .cloneNode(true);
   }
-
-  // _handleLikeIcon() {
-  //   this._likeButton.addEventListener("click", () =>
-  //     this._likeButton.classList.toggle("card__like-button_active")
-  //   );
-  // }
   _handleLikeIcon() {
     this._likeButton.classList.toggle("card__like-button-active");
   }
 
   _handleDeleteCard() {
     this._cardElement.remove();
-    // this._cardElement = null;
+    this._cardElement = null;
   }
 
   _renderCard() {
@@ -35,29 +29,21 @@ export default class Card {
     this._likeButton.addEventListener("click", () => {
       this._handleLikeIcon();
     });
-
-    // card delete button
     this._trashButton.addEventListener("click", () => {
       this._handleDeleteCard();
     });
 
-    this._cardElement
-      .querySelector(".card__image")
-      .addEventListener("click", () => {
-        this._handleImageClick({ name: this._name, link: this._link });
-      });
+    this._cardImage.addEventListener("click", () => {
+      this._handleImageClick({ name: this._name, link: this._link });
+    });
   }
   getNewCard() {
-    // const cardData = { name: this._name, link: this._link };
     this._cardElement = this._getTemplate();
     this._cardTitle = this._cardElement.querySelector(".card__title");
     this._likeButton = this._cardElement.querySelector(".card__like-button");
     this._cardImage = this._cardElement.querySelector(".card__image");
-
-    this._cardImage.addEventListener("click", this._handleImageClick);
     this._trashButton = this._cardElement.querySelector(".card__delete-button");
     this._renderCard();
-    this._cardTitle.textContent = this._name;
     this._setEventListeners();
 
     return this._cardElement;
