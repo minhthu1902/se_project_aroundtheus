d4cb4519f0c8dfeeea465d8f85e73206039dbef4
Thu Thai <70996669+minhthu1902@users.noreply.github.com>
2023-12-08 21:34:12 -0600

    Edit popup and clean unneccesary code from reviewer

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -9,42 +9,21 @@ export default class Popup {
     console.log(this);
     this._popupElement.classList.add("modal_opened");
     document.addEventListener("keyup", this._handleEscClose);
-    // document.addEventListener("mousedown", this._handleImageClick);
   }
-
   close() {
     // console.log(this);
     this._popupElement.classList.remove("modal_opened");
     document.removeEventListener("keyup", this._handleEscClose);
-    // document.removeEventListener("mousedown", this._handleImageClick);
   }
-  // _handleEscClose(e) {
-  //   e.preventDefault();
-  //   if (e.key === "Escape") {
-  //     this.close();
-  //     document.removeEventListener("click", this._handleImageClick);
-  //   }
-  // }
-
   _handleEscClose(e) {
     e.preventDefault();
     if (e.key === "Escape") {
-      this.close();
-      document.removeEventListener("click", this._handleImageClick);
-    }
-  }
-  _handleImageClick(e) {
-    if (
-      e.target.classList.contains("modal_opened") ||
-      e.target.classList.contains("modal__close")
-    ) {
       this.close();
     }
   }
   setEventListeners() {
     // add a click event listener to close icon
     this._popupCloseButton = this._popupElement.querySelector(".modal__close");
-    this._popupCloseButton.addEventListener("click", () => this.close());
 
     this._popupElement.addEventListener("mousedown", (e) => {
       if (
