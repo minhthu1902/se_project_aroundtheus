731401c2688165bf0fae0e6333a15368ff92dddc
Thu Thai <70996669+minhthu1902@users.noreply.github.com>
2024-08-25 17:53:28 -0500

    Fix some minor errors on typo project 9

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,7 +1,7 @@
 export default class Popup {
   constructor({modalSelector}) {
-    this._modalSelector = modalSelector;
-    this._popupElement = document.querySelector(this._modalSelector);
+    // this._modalSelector = modalSelector;
+    this._popupElement = document.querySelector(modalSelector);
     this._handleEscClose = this._handleEscClose.bind(this);
   }
 
--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -3,23 +3,25 @@ export default class PopupWithConfirmation extends Popup {
   constructor(modalSelector, handleFormSubmit) {
     super({ modalSelector });
     // this._modalSelector = modalSelector;
+    this._form = this._popupElement.querySelector(".modal__form");
     this._handleFormSubmit = handleFormSubmit;
-    this._modalButton = this._popupElement.querySelector(".modal__button");
+    this._modalButton = this._form.querySelector(".modal__button");
+
   }
 
-  _removeCard(card) {
-    card.remove();
-    card = null;
-  }
+  // _removeCard(card) {
+  //   card.remove();
+  //   card = null;
+  // }
   setSubmit(handleSubmit) {
     this._handleFormSubmit=handleSubmit;
   }
 
-  setLoading(isLoading) {
+  setLoading(isLoading, text) {
     if(isLoading){
       this._modalButton.textContent = "Deleting...";
     } else {
-      this._modalButton.textContent = "Yes";
+      this._modalButton.textContent = text;
     }
   }
 
